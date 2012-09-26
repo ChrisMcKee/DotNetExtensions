af1d73d330e8ca1f30e674bab05667f1b1cf7374
Dan Fisher <danielfishr@gmail.com>
2012-09-26 22:25:42 +0100

    String to int methods added

---
+++ Tests/DateTimeExtensionTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DotNetExtensions;
+using NUnit.Framework;
+
+// ReSharper disable InconsistentNaming
+namespace Tests
+{
+    [TestFixture]
+    public class DateTimeExtensionTests
+    {
+        [Test]
+        public void CurrentTime()
+        {
+            DateTimeExtensions.CurrentTime = () => new DateTime(2000,1,1);
+
+            Assert.That(DateTimeExtensions.CurrentTime(),Is.EqualTo(new DateTime(2000,1,1)));
+        }
+
+        [Test]
+        public void CurrentTime_AffectsWithin()
+        {
+            DateTimeExtensions.CurrentTime = () => new DateTime(2000, 1, 1);
+
+            var dt = new DateTime(2000, 1, 2);
+
+            Assert.That(dt.Within(2.Days()),Is.True);
+        }
+    }
+}
