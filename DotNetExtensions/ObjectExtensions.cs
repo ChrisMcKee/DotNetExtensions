ab26360e5f6ffa198ff8d353d01408a2b126bd3f
Dan Fisher <danielfishr@gmail.com>
2013-05-17 11:37:51 +0100

    Compilation fix

--- DotNetExtensions/ObjectExtensions.cs
+++ DotNetExtensions/ObjectExtensions.cs
@@ -9,27 +9,7 @@ namespace DotNetExtensions
 {
     public static class ObjectExtensions
     {
-        public static string GetFriendlyName(this Type type)
-        {
-            if (type.IsGenericType)
-            {
-                var sb = new StringBuilder();
-                sb.Append(type.Name.Remove(type.Name.IndexOf('`')));
-                sb.Append("<");
-                Type[] arguments = type.GetGenericArguments();
-                for (int i = 0; i < arguments.Length; i++)
-                {
-                    sb.Append(arguments[i].GetFriendlyName());
-                    if (i + 1 < arguments.Length)
-                    {
-                        sb.Append(", ");
-                    }
-                }
-                sb.Append(">");
-                return sb.ToString();
-            }
-            return type.Name;
-        }
+
 
         public static string GetFriendlyTypeName(this object obj)
         {
