04c0f7eead9bc775e8e999176851254e9b1a97ff
Dan Fisher <danielfishr@gmail.com>
2013-06-03 11:10:56 +0100

    Dynamic linq extensions

---
+++ Tests/LinqExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using DotNetExtensions;
+// ReSharper disable InconsistentNaming
+
+namespace Tests
+{
+    [TestFixture]
+    public class LinqExtensionsTests
+    {
+        Tuple<int,string> one = Tuple.Create(1, "one");
+        Tuple<int,string> two  = Tuple.Create(2, "two");
+        Tuple<int,string> three = Tuple.Create(3, "three");
+
+
+        [Test]
+        public void OrderBy_WorksFine()
+        {
+            var tuples = new[]
+                             {
+                                 three,
+                                 two,
+                                 one
+                             };
+
+            var result = tuples.OrderBy("Item1").ToList();
+
+            for (int i = 1; i <=3; i++)
+            {
+                Assert.That(result[i-1].Item1,Is.EqualTo(i));
+                
+            }
+        }
+
+    }
+}
